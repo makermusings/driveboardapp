2f8054097147b644b8aec2ddc377b203f2116afd
TobiasBodewig <tbodewig@web.de>
2018-11-13 21:08:48 +0100

    homing cycle backmove good now

--- firmware/src/stepper.c
+++ firmware/src/stepper.c
@@ -699,10 +699,10 @@ inline void stepper_homing_cycle() {
   #endif
   // x-axis
   homing_move(-999999*xdir, 0.0, 0.0, 10.0*xdir, 0.0, 0.0, 0.4*CONFIG_SEEKRATE);
-  homing_move(-999999*xdir, 0.0, 0.0, 4.0*xdir, 0.0, 0.0, 0.1*CONFIG_SEEKRATE);
+  homing_move(-999999*xdir, 0.0, 0.0, 2.0*xdir, 0.0, 0.0, 0.1*CONFIG_SEEKRATE);
   // y-axis
   homing_move(0.0, -999999*ydir, 0.0, 0.0, 10.0*ydir, 0.0, 0.4*CONFIG_SEEKRATE);
-  homing_move(0.0, -999999*ydir, 0.0, 0.0, 4.0*ydir, 0.0, 0.1*CONFIG_SEEKRATE);
+  homing_move(0.0, -999999*ydir, 0.0, 0.0, 3.0*ydir, 0.0, 0.1*CONFIG_SEEKRATE);
 
   planner_set_position(0.0, 0.0, 0.0);
   clear_vector(stepper_position);
